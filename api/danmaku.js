14a2c0fa05d672b213a1a407aaf65f8a39509e79
peter <moonlinking@hotmail.com>
2025-06-24 14:39:05 +0800

    it is a form from somebody who remove it

---
+++ api/danmaku.js
@@ -0,0 +1,24 @@
+//TODO 专门为部署到vercel服务器上写的接口
+
+var { parseBilibiliXML } = require("../parseDanmaku");
+
+var {str} = require("../danmakuStr")
+module.exports = (req, res) => {
+  let time = req.query.time;
+
+  let danmaku = parseBilibiliXML(str)
+
+  function cmp(obj1,obj2) {
+      return obj1.time - obj2.time
+  }
+
+  danmaku.sort(cmp);
+
+  let data = [];
+  for (let index in danmaku) {
+    if (Math.abs(danmaku[index].timestamp - time) < 0.5) {
+      data.push(danmaku[index]);
+    }
+  }
+  res.send(JSON.stringify(data));
+};
